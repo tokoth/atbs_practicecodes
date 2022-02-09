c1c5ddeb377ddb443aca6eec09e0091fd95a0345
Tyson Okoth <tysonokoth8@gmail.com>
2022-02-09 09:21:50 +0300

    Fantasy game program solution

--- fantasygame.py
+++ fantasygame.py
@@ -8,4 +8,21 @@
 # in the previous project) and the addedItems parameter is a list like dragonLoot.
 
 
-#Solution
+#Solutions
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item, 0)
+        inventory[item] = inventory[item] + 1
+
+    print("Inventory:")
+    item_total = 0
+    for n, i in inventory.items():
+        print(str(i) + ' ' + n)
+        item_total += i
+    print("Total number of items: " + str(item_total))
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
