bd4054efe2cecd04d5e8bdab964559e1feccf630
Tyson Okoth <tysonokoth8@gmail.com>
2022-02-09 09:20:33 +0300

    Fantasy game program

---
+++ fantasygame.py
@@ -0,0 +1,11 @@
+# List to Dictionary Function for Fantasy Game Inventory
+
+# Imagine that a vanquished dragon’s loot is represented as a list of strings like this:
+# dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+# 
+# Write a function named addToInventory(inventory, addedItems), where the 
+# inventory parameter is a dictionary representing the player’s inventory (like 
+# in the previous project) and the addedItems parameter is a list like dragonLoot.
+
+
+#Solution
